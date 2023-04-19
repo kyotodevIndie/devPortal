61d0045b3cb055f72df7607139aede3d43ba2093
kyotodevindie <kyotodevindie@gmail.com>
2023-04-19 15:28:26 -0300

    fix: tag error

--- src/pages/corporative.tsx
+++ src/pages/corporative.tsx
@@ -3,9 +3,5 @@ import { Box } from "@chakra-ui/react";
 import ServicePage from "@/components/Organisms/ServicePage/ServicePage";
 
 export default function Corporative() {
-  return (
-    <Box>
-      <ServicePage />
-    </Box>
-  );
+  return <Box>{/* <ServicePage /> */}</Box>;
 }
